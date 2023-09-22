1370a45689a786d50feec616936e887c0b747c22
jdheeter <johndheeter@gmail.com>
2023-09-23 00:16:30 +0800

    fix bug

--- src/components/chainUnwrappable.ts
+++
@@ -1,5 +0,0 @@
-import { UnwrappableType } from "lib/ibcHubs"
-import { computed } from "vue"
-import { props } from "./UnwrappableTokens.vue"
-
-export const chainUnwrappable = computed<UnwrappableType[]>(() => unwrappableTokens.filter(el => el.data.hubContract[props.name]))
